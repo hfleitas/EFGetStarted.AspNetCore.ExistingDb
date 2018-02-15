b92213b01963cdef4f678c2187a70700fd235cc0
hiramfleitas <hiramfleitas@hotmail.com>
2018-02-15 17:22:02 -0500

    Added DeleteBlogs.sql to cleanup dataload and reseed table.

---
+++ EFGetStarted.AspNetCore.ExistingDb/Database/DeleteBlogs.sql
@@ -0,0 +1,20 @@
+﻿--  +----------------------------------------+
+--  | create database, tables and load data. |
+--  +----------------------------------------+
+
+if db_id('Blogging') is not null
+begin 
+	use [Blogging];
+	
+	delete from [Blog] where BlogId > 7; 
+
+	dbcc checkident ('Blog')
+	--Checking identity information: current identity value '83', current column value '7'.
+
+	dbcc checkident ('Blog', reseed, 7);  
+
+	select * from [Blog] ;
+	select * from [Post] ;
+
+end
+go
